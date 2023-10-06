a7ee823860a6cb3525164661392008acc04c5d29
dhyan-rai <7003944@spsd.sk.ca>
2023-10-06 10:38:28 -0600

    refactored code

--- 03-ball-object/sketch.js
+++ 03-ball-object/sketch.js
@@ -5,19 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let theBall = {
-  x: 100,
-  y: 100,
-  radius: 25,
-  r: 255,
-  g: 0,
-  b: 0,
-  dx: -4,
-  dy: -3,
-}
+let theBall;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theBall = spawnBall();
 }
 
 function draw() {
@@ -35,20 +27,38 @@ function moveBall() {
   theBall.x += theBall.dx;
   theBall.y += theBall.dy;
 
+  // off the right
   if (theBall.x >= width + theBall.radius) {
-    theBall.x -= width + theBall.radius;
+    theBall.x = 0 - theBall.radius;
   }
 
-  if (theBall.y >= height + theBall.radius) {
-    theBall.y -= height + theBall.radius;
+  // off the bottom
+  if (theBall.y + theBall.radius >= height) {
+    theBall.y = 0 - theBall.radius;
   }
 
+  // off the left
   if (theBall.x <= 0 - theBall.radius) {
-    theBall.x += width + theBall.radius;
+    theBall.x = width + theBall.radius;
   }
 
+  // off the top
   if (theBall.y <= 0 - theBall.radius) {
-    theBall.y += height + theBall.radius;
+    theBall.y = height + theBall.radius;
   }
 
+}
+
+function spawnBall() {
+  let theBall = {
+    x: random(width),
+    y: random(height),
+    radius: random(15, 30),
+    r: random(255),
+    g: random(255),
+    b: random(255),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+  };
+  return theBall;
 }
