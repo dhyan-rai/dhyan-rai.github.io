6c7d007d1cd709c4507ba61219d4a454bd494ce4
dhyan-rai <7003944@spsd.sk.ca>
2023-10-16 12:16:43 -0600

    started work on a new concept

--- 04-arrays-objects-assignment/sketch.js
+++ 04-arrays-objects-assignment/sketch.js
@@ -6,40 +6,48 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let p1;
-let p2;
-let angle = 0;
-let angle2 = 5;
-let time = 0;
-let cirSize = 60;
-let angVel = 0;
-let len = 100;
+// let p1;
+// let p2;
+// let angle = 0;
+// let angle2 = 5;
+// let time = 0;
+// let cirSize = 60;
+// let angVel = 0;
+// let len = 100;
+let x = 0;
+let y = 0;
+let ground;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let ground = loadImage("ground.jpg");
 }
 
 function draw() {
-  //background(220);
-  translate(noise(time) * width, noise(time + 300) * height);
-  push();
-  fill("green");
-  noStroke();
-  circle(0, 0, 20);
-  pop();
-  p1 = p5.Vector.fromAngle(angle, len);
-  p2 = p5.Vector.fromAngle(angle2, len);
-  push();
-  fill("black");
-  circle(p1.x, p1.y, cirSize);
-  circle(p2.x, p2.y, cirSize);
-  pop();
-  angle += 0.12 + angVel;
-  angle2 -= 0.1 + angVel;
-  time += 0.01;
-  cirSize += 0.01;
-  //angVel = random(1, 5) / 10;
-  len += 0.01;
+  x = mouseX;
+  y = mouseY;
+  background(ground);
+  ellipse(x, y, 70, 45);
+  rect(x - 40/2, y + 45/2, 40, 80);
+  //rect();
+  // translate(noise(time) * width, noise(time + 300) * height);
+  // push();
+  // fill("green");
+  // noStroke();
+  // circle(0, 0, 20);
+  // pop();
+  // p1 = p5.Vector.fromAngle(angle, len);
+  // p2 = p5.Vector.fromAngle(angle2, len);
+  // push();
+  // fill("black");
+  // circle(p1.x, p1.y, cirSize);
+  // circle(p2.x, p2.y, cirSize);
+  // pop();
+  // angle += 0.12 + angVel;
+  // angle2 -= 0.1 + angVel;
+  // time += 0.01;
+  // cirSize += 0.01;
+  // len += 0.01;
 }
 
 
