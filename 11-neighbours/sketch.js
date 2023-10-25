1d12894371cbce08b40477d5d5f3271a33dd600e
dhyan-rai <7003944@spsd.sk.ca>
2023-10-25 12:17:37 -0600

    made grid game

---
+++ 11-neighbours/sketch.js
@@ -0,0 +1,84 @@
+// 2D Grid Neighbours
+
+let grid;
+let cellSize;
+const GRID_SIZE = 15;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if(height >= width){
+    cellSize = width/GRID_SIZE;
+  }
+  else {
+    cellSize = height/GRID_SIZE;
+  }
+  grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function mousePressed(){
+  let y = floor(mouseY/cellSize);
+  let x = floor(mouseX/cellSize);
+
+  toggleCell(x, y);
+  toggleCell(x, y-1);
+  toggleCell(x, y+1);
+  toggleCell(x-1, y);
+  toggleCell(x+1, y);
+}
+
+function toggleCell(x, y) {
+  // check that we are within the grid, then toggle
+  if(x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
+    if(grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+  }
+}
+
+function displayGrid() {
+  for(let y = 0; y < GRID_SIZE; y++) {
+    for(let x = 0; x < GRID_SIZE; x++) {
+      if(grid[y][x] === 0) {
+        fill("white");
+      }
+      if(grid[y][x] === 1){
+        fill("black");
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let randomArray = [];
+  for(let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for(let x = 0; x < rows; x++) {
+      if(random(100) < 50) {
+        randomArray[y].push(0);
+      } else {
+        randomArray[y].push(1);
+      }      
+    }
+  }
+  return randomArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let randomArray = [];
+  for(let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for(let x = 0; x < rows; x++) {
+      randomArray[y].push(0);
+    }
+  }
+  return randomArray;
+}
