f97c3a165f1110ed7aa38738e1d885612b410338
dhyan-rai <7003944@spsd.sk.ca>
2023-11-30 11:46:07 -0600

    updated p5play demo

--- 23-p5play/sketch.js
+++ 23-p5play/sketch.js
@@ -3,19 +3,16 @@
 
 let sA, sB, j;
 function setup() {
-	new Canvas(500, 500);
-	world.gravity.y = 10;
-
-	sA = new Sprite(250, 0, 10, 50, 'k');
-	sB = new Sprite(100, 20, 20);
-
-	j = new DistanceJoint(sA, sB);
-	j.offsetA.y = 25;
-
-	j.springiness = 1.0; // try changing this!
+  new Canvas(500, 500);
+  world.gravity.y = 10;  
+  sA = new Sprite(250, 0, 10, 50, 'k');
+  sB = new Sprite(100, 20, 20);  
+  j = new DistanceJoint(sA, sB);
+  j.offsetA.y = 25;  
+  j.springiness = 1.0; // try changing this!
 }
 
 function draw() {
-	clear();
-	sA.moveTowards(mouse);
+  clear();
+  sA.moveTowards(mouse);
 }
